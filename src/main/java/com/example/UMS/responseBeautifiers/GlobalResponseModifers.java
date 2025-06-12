3e9b6a3b4e1ebbff66f52f1d15510388d64423cd
NIKHIL SURE <surenikhil10@gmail.com>
2025-06-13 01:30:32 +0530

    UMS:: GlobalResponseModifier:: fixed bug:: getting ACCEPTED200 for invalid respose

--- src/main/java/com/example/UMS/responseBeautifiers/GlobalResponseModifers.java
+++ src/main/java/com/example/UMS/responseBeautifiers/GlobalResponseModifers.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,8 +14,13 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import jakarta.servlet.http.HttpServletResponse;
+
 @RestControllerAdvice
 public class GlobalResponseModifers implements ResponseBodyAdvice<Object>{
+    @Autowired
+    HttpServletResponse servletResponse;
+
     @Override
     public boolean supports(MethodParameter retunType, Class <? extends HttpMessageConverter<?>> converterType) {
         return true;
@@ -29,7 +35,7 @@ public class GlobalResponseModifers implements ResponseBodyAdvice<Object>{
 
         Map<String, Object> responseBody = new HashMap<>();
         responseBody.put("time", LocalDateTime.now());
-        responseBody.put("status", HttpStatus.ACCEPTED);
+        responseBody.put("status", servletResponse.getStatus());
         responseBody.put("data", body);
         response.setStatusCode(HttpStatus.OK);
         return responseBody;
